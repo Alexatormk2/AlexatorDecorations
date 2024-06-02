26cca1ffc21e5882de67d66d04358e260590e377
alexa <alexatorlulu@gmail.com>
2024-06-02 19:14:17 +0200

    priemr

--- src/main/java/net/alexatorv13/alexatordecorations/AlexatorDecorationsClient.java
+++ src/main/java/net/alexatorv13/alexatordecorations/AlexatorDecorationsClient.java
@@ -3,6 +3,7 @@ package net.alexatorv13.alexatordecorations;
 import net.fabricmc.api.ClientModInitializer;
 
 public class AlexatorDecorationsClient implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
 
--- src/main/java/net/alexatorv13/alexatordecorations/item/ModItems.java
+++
@@ -1,4 +0,0 @@
-package net.alexatorv13.alexatordecorations.item;
-
-public class ModItems {
-}
